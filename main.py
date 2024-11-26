ac924afab749a6a2a570f6a7e6aae6da2de48c1b
JoaoRodrigues230 <joaovp.rodrigues@outlook.com>
2024-11-25 21:33:51 -0300

    Fix: Inserindo funcionalidades da tela da Urna

--- main.py
+++ main.py
@@ -110,3 +110,101 @@ def tela_cadastro():
     btn_limpar.grid(row=7, column=0, columnspan=2, pady=10)
 
     cadastro.mainloop()
+
+#tela da urna eletrônica
+def tela_urna():
+    eleitor_atual = {"titulo": None, "nome": None}#infos do eleitor atual
+    urna_window = tk.Tk()
+    urna_window.title("Urna Eletrônica")
+    urna_window.geometry("600x400")
+    urna_window.resizable(False, False)#para que não redimencione a tela
+
+    #grava o voto digitado
+    voto_digitado = tk.StringVar()
+    
+    #mostra o candidato enquanto o usuário digita
+    candidato_atual = tk.StringVar(value="Candidato:")  
+
+    #configurações de grid para a janela principal
+    urna_window.rowconfigure(0, weight=1)
+    urna_window.columnconfigure(0, weight=1)
+    urna_window.columnconfigure(1, weight=1)
+
+    def iniciar_votacao():
+        #confere o título do eleitor para habilitar a votação
+        try:
+            titulo = int(entry_titulo.get())
+            if titulo in eleitores:
+                eleitor_atual["titulo"] = titulo
+                eleitor_atual["nome"] = eleitores[titulo].get_nome()
+                lbl_eleitor["text"] = f"Eleitor: {eleitor_atual['nome']}"
+                lbl_status["text"] = "Status: Liberado para votar"
+                habilitar_teclado()
+                
+                #limpa o número digitado
+                voto_digitado.set("")  
+            else:
+                raise Exception("Eleitor não encontrado!")
+        except ValueError:
+            messagebox.showerror("Erro", "Título inválido! Insira apenas números.")
+        except Exception as e:
+            lbl_eleitor["text"] = "Eleitor não encontrado!"
+            lbl_status["text"] = f"Status: {e}"
+
+    def registrar_voto():
+        #registra o voto
+        numero = voto_digitado.get()
+        try:
+            if numero.isdigit() and int(numero) in candidatos:
+                candidato = candidatos[int(numero)]
+                votos[int(numero)] = votos.get(int(numero), 0) + 1
+                lbl_status["text"] = f"Voto registrado: {candidato.get_nome()}"
+            elif numero == "BRANCO":
+                votos["BRANCO"] = votos.get("BRANCO", 0) + 1
+                lbl_status["text"] = "Voto registrado: BRANCO"
+            elif numero == "NULO":
+                votos["NULO"] = votos.get("NULO", 0) + 1
+                lbl_status["text"] = "Voto registrado: NULO"
+            else:
+                raise Exception("Número inválido ou candidato não encontrado!")
+            
+            #salva o voto no arquivo .pkl
+            salvar_dados(FILE_VOTOS, votos)             
+            messagebox.showinfo("Sucesso", "Voto registrado com sucesso!")
+            
+            #limpa tela para próximo eleitor
+            limpar_voto()
+        except Exception as e:
+            messagebox.showerror("Erro", str(e))
+
+    def adicionar_numero(numero):
+        #concatena o número digitado nos botões
+        if eleitor_atual["titulo"] is None:
+            messagebox.showwarning("Atenção", "Valide o eleitor antes de votar!")
+            return
+
+        #add o número digitado
+        voto_digitado.set(voto_digitado.get() + str(numero))
+        #atualiza a prévia do voto
+        lbl_preview["text"] = f"Voto: {voto_digitado.get()}"  
+
+        #exibe o nome do candidato
+        try:
+            numero_atual = int(voto_digitado.get())
+            if numero_atual in candidatos:
+                candidato_atual.set(f"Candidato: {candidatos[numero_atual].get_nome()}")
+            else:
+                candidato_atual.set("Candidato: Não encontrado")
+        except ValueError:
+            candidato_atual.set("Candidato: Não encontrado")
+
+    def limpar_voto():
+        #limpa campo de visualização do voto e do candidato
+        voto_digitado.set("")
+        candidato_atual.set("Candidato:")
+        lbl_preview["text"] = "Voto:"
+
+    def habilitar_teclado():
+        #habiliata os botões para votar após a confirmação do eleitor
+        for widget in frame_teclado.winfo_children():
+            widget.config(state="normal")
